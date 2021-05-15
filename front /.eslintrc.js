e83265431fa3cbcff2bbbb6ae9a39d75e183558f
vernonKwon <kwondroid@daum.net>
2021-05-15 14:10:12 +0900

    airbnb 추가 및 충돌 규칙 처리

--- front /.eslintrc.js
+++ front /.eslintrc.js
@@ -5,9 +5,11 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript',
+    'prettier',
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -16,10 +18,26 @@ module.exports = {
     },
     ecmaVersion: 12,
     sourceType: "module",
+    files: ['*.ts', '*.tsx'],
+    project: ['./tsconfig.json'],
   },
   plugins: ["react", "@typescript-eslint", "react-hooks"],
   rules: {
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies},
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies},
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': ['warn'],
+    '@typescript-eslint/semi': 0,
+    '@typescript-eslint/quotes': ['error', 'single'],
+    '@typescript-eslint/comma-dangle': [0],
+    '@typescript-eslint/no-use-before-define': [0],
+    '@typescript-eslint/no-unused-vars': [1],
+    '@typescript-eslint/naming-convention': [1],
+    '@typescript-eslint/no-explicit-any': [1],
+    'import/prefer-default-export': 0,
+    'comma-dangle': 'off',
+    'react/button-has-type': [0],
+    'jsx-a11y/alt-text': [1],
+    'no-console': [2],
   },
 }
